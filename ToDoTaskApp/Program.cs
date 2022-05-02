0ae7fc76671debb1fe2c1e82ca04f1c4d4866d12
Daniel Wilczak <danielmarekwilczak@gmail.com>
2022-05-02 04:24:31 +0200

    Add db connection

--- ToDoTaskApp/Program.cs
+++ ToDoTaskApp/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoTaskApp.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add database connection
+builder.Services.AddDbContext<AppDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("Application")));
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
