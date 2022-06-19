b951a2753c9a2b021a4b274349ca980938d6c320
Daniel Wilczak <d.wilczak@youlead.pl>
2022-06-19 02:40:52 +0200

    Add easynetq nuget

---
+++ ToDoTaskApp/Migrations/20220610231509_DockerMigrate3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoTaskApp.Migrations
+{
+    public partial class DockerMigrate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
