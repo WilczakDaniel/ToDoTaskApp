c80d61cd065a9e95b010864c025ff1f98553b3a3
Daniel Wilczak <d.wilczak@youlead.pl>
2022-07-09 12:48:23 +0200

    AddTaskView

--- WPF/Views/AddTaskView.xaml.cs
+++ WPF/Views/AddTaskView.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+using ToDoTaskApp.Models;
 
 namespace WPF.Views;
 
@@ -8,4 +15,25 @@ public partial class AddTaskView : UserControl
     {
         InitializeComponent();
     }
+
+    private  async void AddTask_Button(object sender, RoutedEventArgs e)
+    {
+        HttpClient client = new HttpClient();
+        string json = JsonConvert.SerializeObject( new ToDoTaskVM() {TaskName = TaskName.Text, TaskDate = Date.SelectedDate.Value,TaskDescription = TaskDescription.Text,TaskCategoryId = Int32.Parse(CategoryId.Text)});
+        var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("http://localhost:5000/api/tasks", stringContent);
+        if (response.IsSuccessStatusCode)
+        {
+            MessageBox.Show("Success");
+            TaskName.Text = "";
+            Date.Text = "";
+            TaskDescription.Text = "";
+            CategoryId.Text = "";
+        }
+        else
+        {
+            MessageBox.Show("Fail");
+        }
+    }
+    
 }
