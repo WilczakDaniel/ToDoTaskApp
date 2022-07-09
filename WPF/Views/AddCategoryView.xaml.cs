e8854c10fc20491b988882723485aee4f4ba7128
Daniel Wilczak <d.wilczak@youlead.pl>
2022-07-09 12:14:30 +0200

    AddCategoryView

--- WPF/Views/AddCategoryView.xaml.cs
+++ WPF/Views/AddCategoryView.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+using ToDoTaskApp.Entities;
+using ToDoTaskApp.Models;
 
 namespace WPF.Views;
 
@@ -8,4 +15,25 @@ public partial class AddCategoryView : UserControl
     {
         InitializeComponent();
     }
+
+    
+    
+    private async void AddCategory_Button(object sender, RoutedEventArgs e)
+    {
+        HttpClient client = new HttpClient();
+        string json = JsonConvert.SerializeObject( new TaskCategoryVM() {Name = Name.Text});
+        var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("http://localhost:5000/api/categories", stringContent);
+        if (response.IsSuccessStatusCode)
+        {
+            MessageBox.Show("Success");
+            Name.Text = "";
+        }
+        else
+        {
+            MessageBox.Show("Fail");
+        }
+    }
+
+    
 }
